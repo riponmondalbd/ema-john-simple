9891116a822bf9db5fc8fefdbc6fbf145f6e3b9a
Ripon Mondal <riponmondal149@gmail.com>
2023-12-04 21:53:58 +0600

    retrive from stored cart and set it to cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -16,8 +16,22 @@ const Shop = () => {
 
     useEffect(() => {
         const storedCart = getShoppingCart();
-        console.log(storedCart);
-    }, [])
+        const savedCart = [];
+        // step 1: get id of the added product
+        for (const id in storedCart) {
+            // step 2: get product from products state by using id
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                // step 3: add quantity
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                // step 4: add the added product  to the  saved cart
+                savedCart.push(addedProduct);
+            }
+        }
+        // step 5: save the cart
+        setCart(savedCart);
+    }, [products])
 
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
