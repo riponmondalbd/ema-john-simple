1fb3e91833620a10acfea7e67e10a80a60a668c9
Ripon Mondal <riponmondal149@gmail.com>
2023-12-31 10:25:02 +0600

    add react router for ema john and set come components with navigation menu linked

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,40 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Shop from './components/Shop/Shop.jsx'
+import Home from './components/Layout/Home.jsx'
+import Orders from './components/Orders/Orders.jsx'
+import Inventory from './components/Inventory/Inventory.jsx'
+import Login from './components/Login/Login.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    children: [
+      {
+        path: '/',
+        element: <Shop />,
+      },
+      {
+        path: 'orders',
+        element: <Orders />,
+      },
+      {
+        path: 'inventory',
+        element: <Inventory />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
